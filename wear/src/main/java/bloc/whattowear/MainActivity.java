3f396ceb3f9883534c30bd277504153ae6d93230
Austin Oestreich <amichaeloestreich@yahoo.com>
2016-01-15 01:57:33 -0600

    CardFragment added

--- wear/src/main/java/bloc/whattowear/MainActivity.java
+++ wear/src/main/java/bloc/whattowear/MainActivity.java
@@ -1,11 +1,11 @@
 package bloc.whattowear;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.wearable.view.CardFragment;
-import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -16,17 +16,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        CardFragment cardFragment = CardFragment.create(getString(R.string.title), "yo");
-        fragmentTransaction.add(R.id.weather_layout, cardFragment);
+        CardFragment cardFragment = CardFragment.create(getString(R.string.title), getString(R.string.desc));
+        fragmentTransaction.add(R.id.w_layout, cardFragment);
         fragmentTransaction.commit();
     }
 }
